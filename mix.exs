127f6762ce5abdd0fc8ba9fb864cbd0ed4ce81f9
Lorenzo Sánchez Fraile <sanchezfrailelorenzo@gmail.com>
2026-03-17 11:19:32 +0100

    update image url on READMEs to use raw url

--- mix.exs
+++ mix.exs
@@ -3,7 +3,6 @@ defmodule Batamanta.MixProject do
 
   @version "1.0.0"
   @source_url "https://github.com/Lorenzo-SF/Batamanta"
-  # Establecemos la línea de corte al nivel de ex_doc (Elixir 1.15 / OTP 24)
   @elixir_vsn "~> 1.15"
 
   def project do
