1a620f3b667c470bd77d8b0fd0ce6bdb95f3d295
Test User <test@example.com>
2026-06-10 22:47:52 +0200

    fix: rename .ex files in test/ to .exs to avoid parallel compiler race condition

--- test/test_helper.exs
+++ test/test_helper.exs
@@ -1,5 +1,5 @@
-Code.require_file("test_httpc.ex", __DIR__)
-Code.require_file("support/runner_mock.ex", __DIR__)
+Code.require_file("test_httpc.exs", __DIR__)
+Code.require_file("support/runner_mock.exs", __DIR__)
 ExUnit.start()
 
 # Skip integration tests by default (they require network)
